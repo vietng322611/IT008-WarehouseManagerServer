8d33253474681a05240a9a2cd0cb4ab0a41fe761
vietng322611 <vietng322611@gmail.com>
2025-10-06 14:39:59 +0700

    Feat: Add WarehouseController

---
+++ Controllers/WarehouseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WarehouseManagerServer.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WarehouseController: Controller
+{
+    
+}
