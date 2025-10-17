cb711668cdc2ddee653ede2d103304b6b59cfb1d
vietng322611 <vietng322611@gmail.com>
2025-10-17 09:18:33 +0700

    Feat: Register services and repositories

---
+++ Extensions/RepositoryExtension.cs
@@ -0,0 +1,18 @@
+﻿using WarehouseManagerServer.Repositories;
+using WarehouseManagerServer.Repositories.Interfaces;
+
+namespace WarehouseManagerServer.Extensions;
+
+public static class RepositoryExtension
+{
+    public static void RegisterRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IMovementRepository, MovementRepository>();
+        services.AddScoped<IPermissionRepository, PermissionRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<ISupplierRepository, SupplierRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IWarehouseRepository, WarehouseRepository>();
+    }
+}
---
+++ Extensions/ServiceExtension.cs
@@ -0,0 +1,17 @@
+﻿using WarehouseManagerServer.Services;
+using WarehouseManagerServer.Services.Interfaces;
+
+namespace WarehouseManagerServer.Extensions;
+
+public static class ServiceExtension
+{
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<IMovementService, MovementService>();
+        services.AddScoped<IPermissionService, PermissionService>();
+        services.AddScoped<ISupplierService, SupplierService>();
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IWarehouseService, WarehouseService>();
+    }
+}
