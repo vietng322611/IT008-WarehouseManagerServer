23cac20309df50caff5ac034560744449965937a
vietng322611 <vietng322611@gmail.com>
2025-11-06 13:22:04 +0700

    Ref: Refactoring

---
+++ Models/Entities/Permission.cs
@@ -0,0 +1,12 @@
+﻿using WarehouseManagerServer.Models.Enums;
+
+namespace WarehouseManagerServer.Models.Entities;
+
+public class Permission
+{
+    public int UserId { get; set; }
+
+    public int WarehouseId { get; set; }
+
+    public ICollection<PermissionEnum> Permissions { get; set; }
+}
