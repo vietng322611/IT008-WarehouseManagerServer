4127c2c999f8d74890ebccc3ffe674b50f14715b
vietng322611 <vietng322611@gmail.com>
2025-11-21 13:21:25 +0700

    feat: add attributes for json serialization

--- Models/DTOs/UserDto.cs
+++ Models/DTOs/UserDto.cs
@@ -1,12 +1,15 @@
-﻿namespace WarehouseManagerServer.Models.DTOs;
+﻿using System.Text.Json.Serialization;
+
+namespace WarehouseManagerServer.Models.DTOs;
 
 public class UserDto
 {
+    [JsonPropertyName(("user_id"))]
     public int UserId { get; set; }
-
+    [JsonPropertyName(("username"))]
     public string Username { get; set; } = null!;
-
+    [JsonPropertyName(("email"))]
     public string Email { get; set; } = null!;
-
+    [JsonPropertyName(("join_date"))]
     public DateTime? JoinDate { get; set; }
 }
