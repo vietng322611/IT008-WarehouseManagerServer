8a740ea08da6fc4b80ffbf95ecc7cd92b3f2edb8
vietng322611 <vietng322611@gmail.com>
2025-11-22 12:54:29 +0700

    feat: add DTOs for password-related methods

---
+++ Models/DTOs/ChangePasswordDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WarehouseManagerServer.Models.DTOs;
+
+public class ChangePasswordDto
+{
+    [JsonPropertyName("old_password")]
+    public string OldPassword { get; set; } = "";
+    [JsonPropertyName("new_password")]
+    public string NewPassword { get; set; } = "";
+}
---
+++ Models/DTOs/ResetPasswordDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WarehouseManagerServer.Models.DTOs;
+
+public class ResetPasswordDto
+{
+    [JsonPropertyName("code")]
+    public string Code { get; set; } = "";
+    [JsonPropertyName("new_password")]
+    public string NewPassword { get; set; } = "";
+}
