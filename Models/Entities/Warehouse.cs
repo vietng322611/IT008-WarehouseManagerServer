97555487800be98fc390271903671a88e8e49cc3
vietng322611 <vietng322611@gmail.com>
2025-11-23 14:31:33 +0700

    feat: ignore unused fields when serializing json

--- Models/Entities/Warehouse.cs
+++ Models/Entities/Warehouse.cs
@@ -7,13 +7,9 @@ public class Warehouse
     [JsonPropertyName("warehouse_id")] public int WarehouseId { get; set; }
     [JsonPropertyName("name")] public string Name { get; set; } = null!;
 
-    public virtual ICollection<Supplier> Suppliers { get; set; } = new List<Supplier>();
-
-    public virtual ICollection<Category> Categories { get; set; } = new List<Category>();
-
-    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
-
-    public virtual ICollection<User> Users { get; set; } = new List<User>();
-
-    public virtual ICollection<Permission> Permissions { get; set; } = new List<Permission>();
+    [JsonIgnore] public virtual ICollection<Supplier> Suppliers { get; set; } = new List<Supplier>();
+    [JsonIgnore] public virtual ICollection<Category> Categories { get; set; } = new List<Category>();
+    [JsonIgnore] public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+    [JsonIgnore] public virtual ICollection<User> Users { get; set; } = new List<User>();
+    [JsonIgnore] public virtual ICollection<Permission> Permissions { get; set; } = new List<Permission>();
 }
