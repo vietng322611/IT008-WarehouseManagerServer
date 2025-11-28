fda523a6aba3879a0e18f8a0fee0227c032e03e0
vietng322611 <vietng322611@gmail.com>
2025-11-28 19:49:35 +0700

    ref: remove the `required` constraint

--- Models/Entities/Category.cs
+++ Models/Entities/Category.cs
@@ -6,7 +6,7 @@ public class Category
 {
     [JsonPropertyName("category_id")] public int CategoryId { get; set; }
     [JsonPropertyName("warehouse_id")] public int WarehouseId { get; set; }
-    [JsonPropertyName("name")] public required string Name { get; set; }
+    [JsonPropertyName("name")] public string Name { get; set; }
 
     [JsonIgnore] public virtual Warehouse Warehouse { get; set; } = null!;
     [JsonIgnore] public virtual ICollection<Product> Products { get; set; } = new List<Product>();
