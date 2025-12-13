c43487cd9cbde6c4f72c29c854b1fbd274013c06
vietng322611 <vietng322611@gmail.com>
2025-12-13 16:46:17 +0700

    feat: add `RemoveCategory` migration

--- Migrations/20251213094547_RemoveCategory.Designer.cs
+++ Migrations/20251213094547_RemoveCategory.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WarehouseManagerServer.Models.DTOs;
@@ -11,9 +12,11 @@ using WarehouseManagerServer.Models.DTOs;
 namespace WarehouseManagerServer.Migrations
 {
     [DbContext(typeof(WarehouseContext))]
-    partial class WarehouseContextModelSnapshot : ModelSnapshot
+    [Migration("20251213094547_RemoveCategory")]
+    partial class RemoveCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
