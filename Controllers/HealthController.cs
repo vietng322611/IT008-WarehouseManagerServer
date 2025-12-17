c57e7dbefbcaa18da3375a3b9c37349abace7133
vietng322611 <vietng322611@gmail.com>
2025-12-17 12:07:19 +0700

    feat: add route `api/health`

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WarehouseManagerServer.Controllers;
+
+[ApiController]
+[Route("api/health")]
+public class HealthController: ControllerBase
+{
+    [HttpGet]
+    [HttpHead]
+    public IActionResult Health()
+    {
+        return Ok();
+    }
+}
